01d4f1fdc23240d1f3b00c8d86ebd2896ca8c150
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-17 18:55:55 -0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    overrides: [
+      {
+        files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+        extends: 'standard-with-typescript',
+        parserOptions: {
+          project: './tsconfig.json'
+        }
+      }
+    ],
+  }
+  
