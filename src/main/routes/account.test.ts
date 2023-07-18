c73e040eaf1188585d69b3205aefb7664948410c
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-18 02:33:40 -0300

    test: ensure Login routes return acessToken on success

--- src/main/routes/account.test.ts
+++ src/main/routes/account.test.ts
@@ -2,10 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
 let accountCollection: Collection
 
-describe('Account routes', () => {
+describe('Post/ Login', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
@@ -23,5 +24,42 @@ describe('Account routes', () => {
       password: '123456',
       passwordConfirmation: '123456'
     }).expect(200)
+  })
+})
+
+describe('POST /login', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection?.deleteMany({})
+  })
+
+  test('should return 200 on login', async () => {
+    const password = await hash('123', 12)
+    await accountCollection.insertOne({
+      username: 'Jefferson',
+      email: 'jeffersonloop14@mail.com',
+      password
+    })
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'jeffersonloop14@mail.com',
+        password: '123'
+      }).expect(200)
+  })
+
+  test('should return 401 on login', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'jeffersonloop14@gmail.com',
+        password: '123'
+      }).expect(401)
   })
 })
