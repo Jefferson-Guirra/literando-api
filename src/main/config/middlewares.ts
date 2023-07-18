ca9f4175121644c6dd46e1b05526c783257d7358
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-18 00:26:29 -0300

    feat: ensure cors enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from '../middlewares/body-parser'
 import { Express } from 'express'
+import { cors } from '../middlewares/cors'
 
 export const setupMiddlewares = (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
