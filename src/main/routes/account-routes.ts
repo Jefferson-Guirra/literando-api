13350bbc245e8135be4eb22f46fc0e1ce472122c
jefferson guirra <jeffersonguirra8@gmail.com>
2023-07-18 01:57:27 -0300

    feat: add expressRouteAdapter

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
+import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { makeSignUpController } from '../factories/account/signup/signup-factory'
 
 const accountRouter = (router: Router): void => {
-  router.post('/signup', (req, res) => res.json({ name: 'test' }))
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
 export default accountRouter
--- src/presentation/helpers/validators/required-field-validator.ts
+++ src/presentation/helpers/validators/required-field-validator.ts
@@ -1,9 +1,10 @@
+/* eslint-disable no-prototype-builtins */
 import { MissingParamError } from '../../errors/missing-params-error'
 import { Validation } from '../../protocols/validate'
 export class RequiredFieldValidator implements Validation {
   constructor (private readonly fieldName: string) {}
   validation (input: any): Error | undefined {
-    if (!Object.prototype.hasOwnProperty.call(input.body, this.fieldName)) {
+    if (!input.body.hasOwnProperty(this.fieldName)) {
       return new MissingParamError(this.fieldName)
     }
   }
