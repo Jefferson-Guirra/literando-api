71ea569a582274dcfbec75dde24d5c65261de566
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-25 20:53:28 -0300

    feat: create get-books route

--- src/main/routes/book-list-routes.ts
+++ src/main/routes/book-list-routes.ts
@@ -3,11 +3,13 @@ import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddBookListController } from '../factories/book-list/add-book-list/add-book-list-factory'
 import { makeGetBookListController } from '../factories/book-list/get-book-list/get-book-list-controller-factory'
 import { makeRemoveBookListController } from '../factories/book-list/remove-book-list/remove-book-list-controller-factory'
+import { makeGetBooksListController } from '../factories/book-list/get-books-list/get-books-list-controller-factory'
 
 const bookListRoute = (router: Router): void => {
   router.post('/add-book', adaptRoute(makeAddBookListController()))
   router.post('/get-book', adaptRoute(makeGetBookListController()))
-  router.post('/remove-book', adaptRoute(makeRemoveBookListController()))
+  router.delete('/remove-book', adaptRoute(makeRemoveBookListController()))
+  router.post('/get-books', adaptRoute(makeGetBooksListController()))
 }
 
 export default bookListRoute
