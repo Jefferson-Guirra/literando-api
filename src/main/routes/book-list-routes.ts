ded2d56ff17d6cf9941dc1c5b6e980d4114eeadf
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-25 16:50:41 -0300

    feat: create get-book route

--- src/main/routes/book-list-routes.ts
+++ src/main/routes/book-list-routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddBookListController } from '../factories/book-list/add-book-list/add-book-list-factory'
+import { makeGetBookListController } from '../factories/book-list/get-book-list/get-book-list-controller-factory'
 
 const bookListRoute = (router: Router): void => {
   router.post('/add-book', adaptRoute(makeAddBookListController()))
+  router.post('/get-book', adaptRoute(makeGetBookListController()))
 }
 
 export default bookListRoute
