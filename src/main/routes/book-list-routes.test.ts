950b26b7d1118ada2b116d4f8bafb6f515c8cfed
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-25 16:52:21 -0300

    test: ensure get-book route return 200 if success

--- src/main/routes/book-list-routes.test.ts
+++ src/main/routes/book-list-routes.test.ts
@@ -76,3 +76,47 @@ describe('Post /add-book', () => {
     }).expect(401)
   })
 })
+
+describe('Post /get-book', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  beforeEach(async () => {
+    booksCollection = await MongoHelper.getCollection('bookList')
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+    await booksCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('should return 200 if success', async () => {
+    const result = await accountsCollection.insertOne({
+      username: 'any_username',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      accessToken: 'any_token'
+    })
+
+    await booksCollection.insertOne({
+      title: 'any_title',
+      description: 'any_description',
+      authors: "['any_author']",
+      price: 0.0,
+      language: 'any_language',
+      publisher: 'any_publisher',
+      publisherDate: 'any_date',
+      imgUrl: 'any_url',
+      queryDoc: result.insertedId.toString() + 'any_id',
+      pageCount: 1,
+      bookId: 'any_id'
+    })
+
+    await request(app).post('/api/get-book').send({ accessToken: 'any_token', bookId: 'any_id' }).expect(200)
+    const promise = await request(app).post('/api/get-book').send({ accessToken: 'any_token', bookId: 'any_id' })
+    const response = await promise.body
+
+    expect(response).toBeTruthy()
+  })
+})
