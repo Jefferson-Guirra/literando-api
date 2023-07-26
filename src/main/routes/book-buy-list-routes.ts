081c52a153040ffff5cb1f04a4f27166d60018f9
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-26 18:02:13 -0300

    feat: create get-buy-books route

--- src/main/routes/book-buy-list-routes.ts
+++ src/main/routes/book-buy-list-routes.ts
@@ -4,12 +4,14 @@ import { makeAddBookBuyListController } from '../factories/book-buy-list/add-boo
 import { makeGetBookBuyListController } from '../factories/book-buy-list/get-book/get-book-buy-list-controller-factory'
 import { makeRemoveBookBuyListController } from '../factories/book-buy-list/remove-book/remove-book-buy-list-controller-factory'
 import { makeUpdateAmountBookBuyListController } from '../factories/book-buy-list/update-amount/update-amount-book-buy-list-controller-factory'
+import { makeGetBuyBooksListController } from '../factories/book-buy-list/get-books/get-books-buy-list-factory'
 
 const buyBookLIst = (route: Router): void => {
   route.post('/add-buy-book', adaptRoute(makeAddBookBuyListController()))
   route.post('/get-buy-book', adaptRoute(makeGetBookBuyListController()))
   route.delete('/remove-buy-book', adaptRoute(makeRemoveBookBuyListController()))
   route.put('/update-amount-buy-book', adaptRoute(makeUpdateAmountBookBuyListController()))
+  route.post('/get-buy-books', adaptRoute(makeGetBuyBooksListController()))
 }
 
 export default buyBookLIst
