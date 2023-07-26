76480a7ad77e3daa2a93f1889858cc4d0e831d91
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-26 18:53:33 -0300

    feat: ensure NexAuthLoginController return 400 if validator return a error

--- src/presentation/controllers/account/login/next-auth-login-controller.ts
+++ src/presentation/controllers/account/login/next-auth-login-controller.ts
@@ -1,4 +1,4 @@
-import { ok } from '../../../helpers/http/http'
+import { badRequest, ok } from '../../../helpers/http/http'
 import { Controller } from '../../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
@@ -6,7 +6,10 @@ import { Validation } from '../../../protocols/validate'
 export class NexAuthLoginController implements Controller {
   constructor (private readonly validator: Validation) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validator.validation(httpRequest)
+    const error = this.validator.validation(httpRequest)
+    if (error) {
+      return badRequest(error)
+    }
     return ok('success')
   }
 }
