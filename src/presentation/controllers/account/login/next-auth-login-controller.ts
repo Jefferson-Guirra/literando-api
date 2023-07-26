f8b0dbb7a3bbd8009851fc06d3aa8ae02cdfada6
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-26 18:48:05 -0300

    feat: ensure NextAuthLoginController call validator with correct values

---
+++ src/presentation/controllers/account/login/next-auth-login-controller.ts
@@ -0,0 +1,12 @@
+import { ok } from '../../../helpers/http/http'
+import { Controller } from '../../../protocols/controller'
+import { HttpRequest, HttpResponse } from '../../../protocols/http'
+import { Validation } from '../../../protocols/validate'
+
+export class NexAuthLoginController implements Controller {
+  constructor (private readonly validator: Validation) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    this.validator.validation(httpRequest)
+    return ok('success')
+  }
+}
