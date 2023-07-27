5d6072d16efa81c6c2c967b26cbc741af97b11b8
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 12:11:57 -0300

    feat: ensure GenerateRandomString call generator with correct values

--- src/utils/usecases/generate-random-string.ts
+++ src/utils/usecases/generate-random-string.ts
@@ -1,15 +1,19 @@
 import { GenerateRandomPassword } from '../protocols/generate-random-password'
+import generator from 'generate-password'
 
 export class GenerateRandomString implements GenerateRandomPassword {
   generate (): string {
-    const chars = '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    const passwordLength = 7
-    let password = ''
+    const password = generator.generate({
+      exclude: '',
+      excludeSimilarCharacters: false,
+      strict: false,
+      lowercase: true,
+      length: 7,
+      numbers: true,
+      symbols: true,
+      uppercase: true
+    })
 
-    for (let i = 0; i < passwordLength; i++) {
-      const randomNumber = Math.floor(Math.random() * chars.length)
-      password += chars.substring(randomNumber, randomNumber + 1)
-    }
     return password
   }
 }
