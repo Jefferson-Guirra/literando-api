3f9f65403cbd410d715c1579a28a8bc917ca8cf5
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 19:15:19 -0300

    test: ensure NextAuthLoginController return 200 on a succeeds

--- src/presentation/controllers/account/login/next-auth-login-controller.spec.ts
+++ src/presentation/controllers/account/login/next-auth-login-controller.spec.ts
@@ -1,6 +1,6 @@
 import { NextAuth, NextAuthAuthentication, nextAuthAuthenticationModel } from '../../../../domain/usecases/account/next-auth-authentication'
 import { MissingParamError } from '../../../errors/missing-params-error'
-import { badRequest, serverError, unauthorized } from '../../../helpers/http/http'
+import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http'
 import { HttpRequest } from '../../../protocols/http'
 import { Validation } from '../../../protocols/validate'
 import { NextAuthLoginController } from './next-auth-login-controller'
@@ -82,5 +82,11 @@ describe('NextAuthLoginController', () => {
     jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(Promise.reject(new Error('')))
     const response = await sut.handle(makeFakeRequest())
     expect(response).toEqual(serverError(new Error('')))
+  })
+
+  test('should return account data on success', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle(makeFakeRequest())
+    expect(response).toEqual(ok({ accessToken: 'any_token', username: 'any_username' }))
   })
 })
