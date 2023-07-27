856fe64d9145776ebb0c2b5ae2bcc3685d3b2d12
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 00:15:59 -0300

    feat: ensure DbNextAuthAddAccount call Hasher with correct value

--- src/data/usecases/account/add-account/db-add-next-auth-account.ts
+++ src/data/usecases/account/add-account/db-add-next-auth-account.ts
@@ -1,5 +1,6 @@
 import { NextAuthAccount } from '../../../../domain/models/account/next-auth-account'
 import { AddNextAuthAccount, AddNextAuthAccountModel } from '../../../../domain/usecases/account/add-next-auth-account'
+import { Hasher } from '../../../protocols/criptography/hasher'
 import { AddNextAuthAccountRepository } from '../../../protocols/db/account/add-next-auth-account-repository'
 import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
 import { GenerateRandomPassword } from '../../../protocols/factories/generate-random-password'
@@ -8,7 +9,8 @@ export class DbNextAuthAddAccount implements AddNextAuthAccount {
   constructor (
     private readonly loadAccount: LoadAccountByEmailRepository,
     private readonly addAccount: AddNextAuthAccountRepository,
-    private readonly randomPassword: GenerateRandomPassword
+    private readonly randomPassword: GenerateRandomPassword,
+    private readonly Hasher: Hasher
   ) {}
 
   async add (accountModel: AddNextAuthAccountModel): Promise<NextAuthAccount | null> {
@@ -18,6 +20,7 @@ export class DbNextAuthAddAccount implements AddNextAuthAccount {
     }
 
     const password = this.randomPassword.generate()
+    await this.Hasher.hash(password)
     await this.addAccount.addNextAuthAccount({ ...accountModel, password })
     return {
       email: 'any_mail@email.com',
