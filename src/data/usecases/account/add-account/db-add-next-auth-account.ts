017979cf439c1141c0a3e9da4fafd9a8a3a901b9
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-26 22:33:04 -0300

    feat: ensure DbNextAuthAddAccount return null if account exist

--- src/data/usecases/account/add-account/db-add-next-auth-account.ts
+++ src/data/usecases/account/add-account/db-add-next-auth-account.ts
@@ -1,11 +1,20 @@
 import { NextAuthAccount } from '../../../../domain/models/account/next-auth-account'
-import { AddNextAuthAccount } from '../../../../domain/usecases/account/add-next-auth-account'
+import { AddNextAuthAccount, AddNextAuthAccountModel } from '../../../../domain/usecases/account/add-next-auth-account'
 import { LoadAccountByAccessTokenRepository } from '../../../protocols/db/account/load-account-by-access-token-repository'
 
 export class DbNextAuthAddAccount implements AddNextAuthAccount {
   constructor (private readonly loadAccount: LoadAccountByAccessTokenRepository) {}
-  async add (accountModel: NextAuthAccount): Promise<NextAuthAccount | null> {
-    await this.loadAccount.loadByAccessToken(accountModel.accessToken)
-    return null
+  async add (accountModel: AddNextAuthAccountModel): Promise<NextAuthAccount | null> {
+    const account = await this.loadAccount.loadByAccessToken(accountModel.accessToken)
+    if (account) {
+      return null
+    }
+    return {
+      email: 'any_mail@email.com',
+      accessToken: 'any_token',
+      password: 'hashed_password',
+      username: 'any_name',
+      id: 'any_id'
+    }
   }
 }
