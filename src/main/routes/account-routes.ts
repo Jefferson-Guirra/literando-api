08f7e5594037313ad560730a3b68c353ad198bd2
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 15:35:57 -0300

    feat: create next-auth-signup route

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -3,9 +3,11 @@ import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeSignUpController } from '../factories/account/signup/signup-factory'
 import { makeLoginController } from '../factories/account/login/login-controller'
 import { makeLogoutController } from '../factories/account/logout/logout-controller'
+import { makeNextAuthSignupController } from '../factories/account/signup/next-auth-signup-factory'
 
 const accountRouter = (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/next-auth-signup', adaptRoute(makeNextAuthSignupController()))
   router.post('/login', adaptRoute(makeLoginController()))
   router.post('/logout', adaptRoute(makeLogoutController()))
 }
