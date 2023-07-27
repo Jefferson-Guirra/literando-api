89c7f4cc80e453161d6c83f1678d48579e3a8305
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-26 22:43:36 -0300

    test: ensure DbNextAuthAddAccount return null if account exist

--- src/data/usecases/account/add-account/db-add-next-auth-account.spec.ts
+++ src/data/usecases/account/add-account/db-add-next-auth-account.spec.ts
@@ -1,7 +1,16 @@
 import { AccountModel } from '../../../../domain/models/account/account'
 import { AddNextAuthAccountModel } from '../../../../domain/usecases/account/add-next-auth-account'
+import { accountLoginModel } from '../../../protocols/db/account/load-account-by-access-token-repository'
 import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
 import { DbNextAuthAddAccount } from './db-add-next-auth-account'
+
+const makeFakeAccount = (): accountLoginModel => ({
+  email: 'any_email@mail.com',
+  accessToken: 'any_token',
+  password: 'hashed_password',
+  username: 'any_name',
+  id: 'any_id'
+})
 
 const makeFakeRequest = (): AddNextAuthAccountModel => ({
   email: 'any_email@mail.com',
@@ -39,5 +48,12 @@ describe('DbNextAuthAddAccount', () => {
     const loadSpy = jest.spyOn(loadAccountStub, 'loadByEmail')
     await sut.add(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+
+  test('should return null if account exist', async () => {
+    const { sut, loadAccountStub } = makeSut()
+    jest.spyOn(loadAccountStub, 'loadByEmail').mockReturnValueOnce(Promise.resolve(makeFakeAccount()))
+    const response = await sut.add(makeFakeRequest())
+    expect(response).toBeFalsy()
   })
 })
