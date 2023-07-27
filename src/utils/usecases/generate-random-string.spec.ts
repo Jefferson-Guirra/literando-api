e275f57dd6d7d0cfb7a6e660f00c83113a1cc321
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 12:04:04 -0300

    test: ensure GenerateRandomString return random password if generate succeeds

--- src/utils/usecases/generate-random-string.spec.ts
+++ src/utils/usecases/generate-random-string.spec.ts
@@ -1,12 +1,15 @@
 import { GenerateRandomString } from './generate-random-string'
+import generator from 'generate-password'
 
 const makeSut = (): GenerateRandomString => new GenerateRandomString()
 
 describe('GenerateRandomString', () => {
   test('should return random password if success', () => {
     const sut = makeSut()
+    jest.spyOn(generator, 'generate').mockReturnValueOnce('1234567')
     const password = sut.generate()
     expect(password).toBeTruthy()
+    expect(password).toBe('1234567')
     expect(password.length).toBe(7)
   })
 
