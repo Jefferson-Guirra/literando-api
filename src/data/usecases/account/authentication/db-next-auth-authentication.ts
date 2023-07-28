ec1fe1273a05ae5024daa2bb5e0da93540023ecf
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 21:43:15 -0300

    feat: ensure DbNextAuthAuthentication call LoadAccountByEmailRepository with correct email

--- src/data/usecases/account/authentication/db-next-auth-authentication.ts
+++ src/data/usecases/account/authentication/db-next-auth-authentication.ts
@@ -1,14 +1,23 @@
 import { NextAuth, NextAuthAuthentication, nextAuthAuthenticationModel } from '../../../../domain/usecases/account/next-auth-authentication'
+import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
 import { LoadPrivateRouteByNameRepository } from '../../../protocols/db/private-route/load-private-route-by-name-repository'
 
 export class DbNextAuthAuthentication implements NextAuthAuthentication {
-  constructor (private readonly loadPrivateRoute: LoadPrivateRouteByNameRepository) {}
+  constructor (
+    private readonly loadPrivateRoute: LoadPrivateRouteByNameRepository,
+    private readonly loadAccount: LoadAccountByEmailRepository
+  ) {}
+
   async auth (data: nextAuthAuthenticationModel): Promise<NextAuth | null | undefined> {
-    const { routeName, privateKey } = data
+    const { routeName, privateKey, email } = data
     const route = await this.loadPrivateRoute.loadByRouteName(routeName)
     if (!route || privateKey !== route.privateKey) {
       return undefined
     }
-    return null
+    await this.loadAccount.loadByEmail(email)
+    return {
+      accessToken: 'any_token',
+      username: 'any_username'
+    }
   }
 }
