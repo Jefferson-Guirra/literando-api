1ba8b022df28ac30465662bd2939201f8949da96
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-28 15:30:31 -0300

    test: esnure PrivateRouteMongoRepository return route if LoadByRouteName success

---
+++ src/infra/db/private-route/private-route-mongo-repository.spec.ts
@@ -0,0 +1,30 @@
+import { Collection } from 'mongodb'
+import { PrivateRouteMongoRepository } from './private-route-mongo-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+let privateRoutesCollection: Collection
+const insertPrivateRoute = async (): Promise<void> => {
+  await privateRoutesCollection.insertOne({
+    routeName: 'any_name',
+    privateKey: 'any_key'
+  })
+}
+const makeSut = (): PrivateRouteMongoRepository => new PrivateRouteMongoRepository()
+describe('PrivateRouteMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  beforeEach(async () => {
+    privateRoutesCollection = await MongoHelper.getCollection('privateRoutes')
+    await privateRoutesCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('should return privateRoute on succeeds', async () => {
+    const sut = makeSut()
+    await insertPrivateRoute()
+    const response = await sut.loadByRouteName('any_name')
+    expect(response).toEqual({ routeName: 'any_name', privateKey: 'any_key' })
+  })
+})
