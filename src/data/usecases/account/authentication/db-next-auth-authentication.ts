252c0934945fe1f783c4cac7433e5f9d472966a5
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 22:11:28 -0300

    feat: ensure DbNextAuthAuthentication call updateAccessTokenRepository with correct values

--- src/data/usecases/account/authentication/db-next-auth-authentication.ts
+++ src/data/usecases/account/authentication/db-next-auth-authentication.ts
@@ -1,15 +1,17 @@
 import { NextAuth, NextAuthAuthentication, nextAuthAuthenticationModel } from '../../../../domain/usecases/account/next-auth-authentication'
 import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
+import { UpdateAccessTokenRepository } from '../../../protocols/db/account/update-acess-token-repository'
 import { LoadPrivateRouteByNameRepository } from '../../../protocols/db/private-route/load-private-route-by-name-repository'
 
 export class DbNextAuthAuthentication implements NextAuthAuthentication {
   constructor (
     private readonly loadPrivateRoute: LoadPrivateRouteByNameRepository,
-    private readonly loadAccount: LoadAccountByEmailRepository
+    private readonly loadAccount: LoadAccountByEmailRepository,
+    private readonly updateAccessToken: UpdateAccessTokenRepository
   ) {}
 
   async auth (data: nextAuthAuthenticationModel): Promise<NextAuth | null | undefined> {
-    const { routeName, privateKey, email } = data
+    const { routeName, privateKey, email, accessToken } = data
     const route = await this.loadPrivateRoute.loadByRouteName(routeName)
     if (!route || privateKey !== route.privateKey) {
       return undefined
@@ -18,6 +20,7 @@ export class DbNextAuthAuthentication implements NextAuthAuthentication {
     if (!account) {
       return undefined
     }
+    await this.updateAccessToken.update(account.id, accessToken)
     return {
       accessToken: 'any_token',
       username: 'any_username'
