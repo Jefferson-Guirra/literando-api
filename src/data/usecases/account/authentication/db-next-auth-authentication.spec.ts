e8b506da7feaba1bdcce6c42a7014a57cf561d46
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 21:04:10 -0300

    test: ensure DbNextAuthAuthentication call LoadPrivateRoute correct routeName

---
+++ src/data/usecases/account/authentication/db-next-auth-authentication.spec.ts
@@ -0,0 +1,40 @@
+import { nextAuthAuthenticationModel } from '../../../../domain/usecases/account/next-auth-authentication'
+import { LoadPrivateRouteByNameRepository, PrivateRouteModel } from '../../../protocols/db/private-route/load-private-route-by-name-repository'
+import { DbNextAuthAuthentication } from './db-next-auth-authentication'
+
+const makeFakeRequest = (): nextAuthAuthenticationModel => ({
+  routeName: 'any_name',
+  privateKey: 'any_key',
+  email: 'any_email',
+  accessToken: 'any_token'
+})
+const makeLoadPrivateRouteStub = (): LoadPrivateRouteByNameRepository => {
+  class LoadPrivateRouteByNameRepositoryStub implements LoadPrivateRouteByNameRepository {
+    async loadByRouteName (routeName: string): Promise<PrivateRouteModel | null> {
+      return await Promise.resolve({ routeName: 'any_name', privateKey: '' })
+    }
+  }
+  return new LoadPrivateRouteByNameRepositoryStub()
+}
+interface SutTypes {
+  sut: DbNextAuthAuthentication
+  loadPrivateRouteStub: LoadPrivateRouteByNameRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadPrivateRouteStub = makeLoadPrivateRouteStub()
+  const sut = new DbNextAuthAuthentication(loadPrivateRouteStub)
+  return {
+    loadPrivateRouteStub,
+    sut
+  }
+}
+
+describe('DbNextAuthAuthentication', () => {
+  test('should call loadPrivateRoute with correct values', async () => {
+    const { sut, loadPrivateRouteStub } = makeSut()
+    const loadSpy = jest.spyOn(loadPrivateRouteStub, 'loadByRouteName')
+    await sut.auth(makeFakeRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any_name')
+  })
+})
