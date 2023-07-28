70485b7072fed60c41b40953b7bee3899ab53fba
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 21:10:35 -0300

    feat: ensure DbNextAuthAuthentication return undefined if LoadPrivateRoute fails

--- src/data/usecases/account/authentication/db-next-auth-authentication.ts
+++ src/data/usecases/account/authentication/db-next-auth-authentication.ts
@@ -5,7 +5,10 @@ export class DbNextAuthAuthentication implements NextAuthAuthentication {
   constructor (private readonly loadPrivateRoute: LoadPrivateRouteByNameRepository) {}
   async auth (data: nextAuthAuthenticationModel): Promise<NextAuth | null | undefined> {
     const { routeName } = data
-    await this.loadPrivateRoute.loadByRouteName(routeName)
+    const route = await this.loadPrivateRoute.loadByRouteName(routeName)
+    if (!route) {
+      return undefined
+    }
     return null
   }
 }
