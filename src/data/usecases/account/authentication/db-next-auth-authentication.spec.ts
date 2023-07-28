cf4ce776d2a70bb73bed1bb9e43f18b47bcb95c8
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-27 21:44:25 -0300

    test: ensure DbNextAuthAuthentication call LoadAccountByEmailRepository with correct email

--- src/data/usecases/account/authentication/db-next-auth-authentication.spec.ts
+++ src/data/usecases/account/authentication/db-next-auth-authentication.spec.ts
@@ -1,30 +1,50 @@
+import { AccountModel } from '../../../../domain/models/account/account'
 import { nextAuthAuthenticationModel } from '../../../../domain/usecases/account/next-auth-authentication'
+import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
 import { LoadPrivateRouteByNameRepository, PrivateRouteModel } from '../../../protocols/db/private-route/load-private-route-by-name-repository'
 import { DbNextAuthAuthentication } from './db-next-auth-authentication'
 
+const makeFakeAccount = (): AccountModel => ({
+  username: 'any_username',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+  id: 'any_id'
+})
 const makeFakeRequest = (): nextAuthAuthenticationModel => ({
   routeName: 'any_name',
   privateKey: 'any_key',
-  email: 'any_email',
+  email: 'any_email@mail.com',
   accessToken: 'any_token'
 })
 const makeLoadPrivateRouteStub = (): LoadPrivateRouteByNameRepository => {
   class LoadPrivateRouteByNameRepositoryStub implements LoadPrivateRouteByNameRepository {
     async loadByRouteName (routeName: string): Promise<PrivateRouteModel | null> {
-      return await Promise.resolve({ routeName: 'any_name', privateKey: '' })
+      return await Promise.resolve({ routeName: 'any_name', privateKey: 'any_key' })
     }
   }
   return new LoadPrivateRouteByNameRepositoryStub()
 }
+
+const makeLoadAccountRepositoryStub = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    async loadByEmail (email: string): Promise<AccountModel | null> {
+      return await Promise.resolve(makeFakeAccount())
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub()
+}
 interface SutTypes {
-  sut: DbNextAuthAuthentication
+  loadAccountStub: LoadAccountByEmailRepository
   loadPrivateRouteStub: LoadPrivateRouteByNameRepository
+  sut: DbNextAuthAuthentication
 }
 
 const makeSut = (): SutTypes => {
+  const loadAccountStub = makeLoadAccountRepositoryStub()
   const loadPrivateRouteStub = makeLoadPrivateRouteStub()
-  const sut = new DbNextAuthAuthentication(loadPrivateRouteStub)
+  const sut = new DbNextAuthAuthentication(loadPrivateRouteStub, loadAccountStub)
   return {
+    loadAccountStub,
     loadPrivateRouteStub,
     sut
   }
@@ -60,5 +80,12 @@ describe('DbNextAuthAuthentication', () => {
     jest.spyOn(loadPrivateRouteStub, 'loadByRouteName').mockReturnValueOnce(Promise.reject(new Error('')))
     const response = sut.auth(makeFakeRequest())
     await expect(response).rejects.toThrow()
+  })
+
+  test('should call loadAccount with correct email', async () => {
+    const { sut, loadAccountStub } = makeSut()
+    const loadSpy = jest.spyOn(loadAccountStub, 'loadByEmail')
+    await sut.auth(makeFakeRequest())
+    expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
