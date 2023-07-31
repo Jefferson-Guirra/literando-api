25986538ea4a419cefb701030b9b2e1f232bc5bc
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-30 23:35:04 -0300

    test: ensure NodemailerAdapter call NodemailerTransporter with correct email

---
+++ src/infra/email/nodemailer-adapter/nodemail-adapter.spec.ts
@@ -0,0 +1,33 @@
+import { NodemailerAdapter } from './nodemailer-adapter'
+import { NodemailerTransporter } from './protocols/nodemailer-transporter'
+
+const makeTransporterStub = (): NodemailerTransporter => {
+  class NodemailerTransporterStub implements NodemailerTransporter {
+    async active (email: string): Promise<void> {
+
+    }
+  }
+  return new NodemailerTransporterStub()
+}
+interface SutTypes {
+  nodemailerTransporterStub: NodemailerTransporter
+  sut: NodemailerAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const nodemailerTransporterStub = makeTransporterStub()
+  const sut = new NodemailerAdapter(nodemailerTransporterStub)
+  return {
+    nodemailerTransporterStub,
+    sut
+  }
+}
+
+describe('NodemailerAdapter', () => {
+  test('should  call nodemailerTRansporter with correct email', async () => {
+    const { sut, nodemailerTransporterStub } = makeSut()
+    const activeSpy = jest.spyOn(nodemailerTransporterStub, 'active')
+    await sut.sendEmail('any_email@mail.com')
+    expect(activeSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
+})
