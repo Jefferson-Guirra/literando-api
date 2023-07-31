5e9ae1f6ab379202364fab8380122418cafeaece
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-31 09:38:35 -0300

    feat: ensure DbResetPasswordEmail call Encrypter with correctid

--- src/data/usecases/email/reset-password/db-reset-password-email.ts
+++ src/data/usecases/email/reset-password/db-reset-password-email.ts
@@ -1,11 +1,14 @@
 import { ResetPasswordEmail, ResetPasswordEmailModel } from '../../../../domain/usecases/email/reset-pasword-email'
+import { Encrypter } from '../../../protocols/criptography/encrypter'
 import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
 import { SendMessage } from '../../../protocols/email/send-message'
 
 export class DbResetPasswordEmail implements ResetPasswordEmail {
   constructor (
     private readonly loadAccount: LoadAccountByEmailRepository,
-    private readonly SendMessage: SendMessage
+    private readonly SendMessage: SendMessage,
+    private readonly encrypter: Encrypter
+
   ) {}
 
   async reset (email: string): Promise<ResetPasswordEmailModel | null> {
@@ -14,6 +17,7 @@ export class DbResetPasswordEmail implements ResetPasswordEmail {
       return null
     }
     const { email: emailMessage, id } = account
+    await this.encrypter.encrypt(id)
     await this.SendMessage.sendEmail(email)
     return {
       email: emailMessage,
