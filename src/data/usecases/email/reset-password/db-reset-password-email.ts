fa19ccae6599a7ad8e76404bafd9fe3acfa7f29e
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-07-31 10:01:05 -0300

    feat: ensure DbREsetPasswordEmail call GetResetPasswordRequest with correct email

--- src/data/usecases/email/reset-password/db-reset-password-email.ts
+++ src/data/usecases/email/reset-password/db-reset-password-email.ts
@@ -1,13 +1,15 @@
 import { ResetPasswordEmail, ResetPasswordEmailModel } from '../../../../domain/usecases/email/reset-pasword-email'
 import { Encrypter } from '../../../protocols/criptography/encrypter'
 import { LoadAccountByEmailRepository } from '../../../protocols/db/account/load-account-by-email-repository'
+import { GetResetPasswordRequest } from '../../../protocols/email/get-reset-password-request'
 import { SendMessage } from '../../../protocols/email/send-message'
 
 export class DbResetPasswordEmail implements ResetPasswordEmail {
   constructor (
     private readonly loadAccount: LoadAccountByEmailRepository,
     private readonly SendMessage: SendMessage,
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly getRequest: GetResetPasswordRequest
 
   ) {}
 
@@ -18,6 +20,7 @@ export class DbResetPasswordEmail implements ResetPasswordEmail {
     }
     const { email: emailMessage, id } = account
     await this.encrypter.encrypt(id)
+    await this.getRequest.find(email)
     await this.SendMessage.sendEmail(email)
     return {
       email: emailMessage,
