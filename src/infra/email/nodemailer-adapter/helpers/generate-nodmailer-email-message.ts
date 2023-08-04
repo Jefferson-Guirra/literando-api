366bf068e47ec68cfbd3ef5d0364d17b0c3fc32b
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-03 22:40:10 -0300

    feat: ensure  NodemailerAdapter call transporterEmail with correct message

---
+++ src/infra/email/nodemailer-adapter/helpers/generate-nodmailer-email-message.ts
@@ -0,0 +1,13 @@
+import { MailOptions } from 'nodemailer/lib/json-transport'
+
+export const GenerateEmailMessage = {
+  generateResetPasswordMessage (serviceEmail: string, userEmail: string, username: string, url: string, accessToken: string): MailOptions {
+    const redirectUrl = `${url}/ResetPassword/${accessToken}`.toString()
+    return {
+      from: serviceEmail,
+      to: userEmail,
+      subject: `Solicitação de mudança de senha para o usuàrio ${username}`,
+      html: `<div><h2>Literando - Mudança de senha</h2><p>Clique no link a seguir para mudar a senha</p><p>${redirectUrl}</p><p>link válido por 60 segundos.</p></div>`
+    }
+  }
+}
--- src/infra/email/nodemailer-adapter/nodemailer-adapter.ts
+++ src/infra/email/nodemailer-adapter/nodemailer-adapter.ts
@@ -1,12 +1,22 @@
 import { SendResetPasswordMessage } from '../../../data/protocols/email/send-reset-password-message'
 import { NodemailerTransporter } from './protocols/nodemailer-transporter'
+import { GenerateEmailMessage } from './helpers/generate-nodmailer-email-message'
 
 export class NodemailerAdapter implements SendResetPasswordMessage {
   constructor (
+    private readonly serviceEmail: string,
+    private readonly appUrl: string,
     private readonly transporterEmail: NodemailerTransporter
   ) {}
 
   async sendResetPasswordEmail (email: string, username: string, accessToken: string): Promise<void> {
-    await this.transporterEmail.active(email)
+    const message = GenerateEmailMessage.generateResetPasswordMessage(
+      this.serviceEmail,
+      email,
+      username,
+      this.appUrl,
+      accessToken
+    )
+    await this.transporterEmail.active(message)
   }
 }
