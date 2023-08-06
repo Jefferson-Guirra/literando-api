fd19016a873ea2de47b21abee7fac8a71d37ff28
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-06 18:04:43 -0300

    feat: ensure NodemailerGmailTransporter call createTransport with correct values

--- src/infra/email/nodemailer-adapter/usecases/nodemailer-transporter/nodemailer-gmail-transporter.ts
+++ src/infra/email/nodemailer-adapter/usecases/nodemailer-transporter/nodemailer-gmail-transporter.ts
@@ -1,5 +1,7 @@
+import SMTPConnection from 'nodemailer/lib/smtp-connection'
 import { GetOauthAccessToken } from '../../protocols/get-oauth-access-token'
 import { NodemailerTransporter } from '../../protocols/nodemailer-transporter'
+import nodemailer from 'nodemailer'
 import { MailOptions } from 'nodemailer/lib/sendmail-transport'
 
 export interface GmailData {
@@ -12,6 +14,18 @@ export interface GmailData {
 export class NodemailerGmailTransporter implements NodemailerTransporter {
   constructor (private readonly gmail: GmailData, private readonly getAccessToken: GetOauthAccessToken) {}
   async active (message: MailOptions): Promise<void> {
-    await this.getAccessToken.get(this.gmail.clientId, this.gmail.clientSecret, this.gmail.refreshToken)
+    const { accessToken } = await this.getAccessToken.get(this.gmail.clientId, this.gmail.clientSecret, this.gmail.refreshToken)
+    const auth: SMTPConnection.AuthenticationTypeOAuth2 = {
+      type: 'OAuth2',
+      user: this.gmail.serviceEmail,
+      clientId: this.gmail.clientId,
+      clientSecret: this.gmail.clientSecret,
+      refreshToken: this.gmail.refreshToken,
+      accessToken
+    }
+    nodemailer.createTransport({
+      service: 'gmail',
+      auth
+    })
   }
 }
