292bb6170bed9fe2814e7821cb54302e90fa83d8
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-06 22:28:52 -0300

    test: ensure GetOauthToken return throw if invalid clientId provided

--- src/infra/email/nodemailer-adapter/usecases/get-access-token/get-oauth-token.spec.ts
+++ src/infra/email/nodemailer-adapter/usecases/get-access-token/get-oauth-token.spec.ts
@@ -7,4 +7,9 @@ describe('GetOauthToken', () => {
     const response = await sut.getToken(env.googleClientId as string, env.googleSecret as string, env.googleRefreshToken as string)
     expect(response).toBeTruthy()
   })
+  test('should return throw if invalid clientId provided)', async () => {
+    const sut = new GetOauthToken()
+    const promise = sut.getToken('any_id', env.googleSecret as string, env.googleRefreshToken as string)
+    await expect(promise).rejects.toThrow()
+  })
 })
