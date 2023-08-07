0d1e01c45f9858d63a9f8b461ee204afe4889b25
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-07 18:09:42 -0300

    test: ensure ResetPasswordAccountRepository return account data if find success

--- src/infra/db/email/reset-password-account-repository.spec.ts
+++ src/infra/db/email/reset-password-account-repository.spec.ts
@@ -16,13 +16,22 @@ describe('ResetPasswordAccountRepository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
-  test('should return data account if add success', async () => {
+  test('should return account data if add success', async () => {
     const sut = makeSut()
     let count = await resetPasswordAccountsCollection.countDocuments()
     expect(count).toBe(0)
     const response = await sut.add('any_email@mail.com', 'any_token')
     count = await resetPasswordAccountsCollection.countDocuments()
     expect(count).toBe(1)
+    expect(response).toBeTruthy()
+    expect(response?.id).toBeTruthy()
+    expect(response?.email).toBe('any_email@mail.com')
+    expect(response?.accessToken).toBe('any_token')
+  })
+  test('should return account data if GetResetPasswordRequestRepository success', async () => {
+    const sut = makeSut()
+    await resetPasswordAccountsCollection.insertOne({ email: 'any_email@mail.com', accessToken: 'any_token' })
+    const response = await sut.find('any_email@mail.com')
     expect(response).toBeTruthy()
     expect(response?.id).toBeTruthy()
     expect(response?.email).toBe('any_email@mail.com')
