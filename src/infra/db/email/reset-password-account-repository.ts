8da3b1e4a662ac0649e803d4023f66bf3a3ca6d7
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-07 18:08:17 -0300

    feat: ensure ResetPasswordAccountRepository return account data if find success

--- src/infra/db/email/reset-password-account-repository.ts
+++ src/infra/db/email/reset-password-account-repository.ts
@@ -1,12 +1,18 @@
 import { AddResetPasswordRequestRepository } from '../../../data/protocols/db/email/add-reset-password-request-repository'
-import { ResetPasswordRequestModel } from '../../../data/protocols/db/email/get-reset-password-request-repository'
+import { GetResetPasswordRequestRepository, ResetPasswordRequestModel } from '../../../data/protocols/db/email/get-reset-password-request-repository'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class ResetPasswordAccountRepository implements AddResetPasswordRequestRepository {
+export class ResetPasswordAccountRepository implements AddResetPasswordRequestRepository, GetResetPasswordRequestRepository {
   async add (email: string, accessToken: string): Promise<ResetPasswordRequestModel | null> {
     const resetCollection = await MongoHelper.getCollection('resetPasswordAccounts')
     const result = await resetCollection.insertOne({ email, accessToken })
     const doc = await resetCollection.findOne({ _id: result.insertedId })
+    return doc && MongoHelper.Map(doc)
+  }
+
+  async find (email: string): Promise<ResetPasswordRequestModel | null> {
+    const resetCollection = await MongoHelper.getCollection('resetPasswordAccounts')
+    const doc = await resetCollection.findOne({ email })
     return doc && MongoHelper.Map(doc)
   }
 }
