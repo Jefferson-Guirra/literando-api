a1f9611b77f22d08dcbefd95aca164d37aa261cb
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-07 21:10:44 -0300

    feat: create ResetPasswordEmailController to a factory method

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,6 +4,8 @@ export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/literando',
   port: process.env.port || 8080,
   jwtSecret: process.env.JWT_SECRET || 'rjlkm856LjCvAe158WKl-=ER=Kmnw/',
+  serviceEmail: process.env.SERVICE_EMAIL as string,
+  appUrl: process.env.APP_URL || 'http://localhost:3000',
   googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleSecret: process.env.GOOGLE_SECRET,
   googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN
---
+++ src/main/factories/email/reset-password/reset-password-email-controller-factory.ts
@@ -0,0 +1,36 @@
+import { DbResetPasswordEmail } from '../../../../data/usecases/email/reset-password/db-reset-password-email'
+import { JwtAdapter } from '../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { AccountMongoRepository } from '../../../../infra/db/account/account-mongo-repository'
+import { ResetPasswordAccountRepository } from '../../../../infra/db/email/reset-password-account-repository'
+import { NodemailerAdapter } from '../../../../infra/email/nodemailer-adapter/nodemailer-adapter'
+import { ResetPasswordEmailController } from '../../../../presentation/controllers/email/reset-pasword/reset-password-email-controller'
+import { makeResetPasswordEmailValidator } from './reset-password-email-validator-factory'
+import env from '../../../config/env'
+import { Controller } from '../../../../presentation/protocols/controller'
+import { GmailData, NodemailerGmailTransporter } from '../../../../infra/email/nodemailer-adapter/usecases/nodemailer-transporter/nodemailer-gmail-transporter'
+import { GetOauthToken } from '../../../../infra/email/nodemailer-adapter/usecases/get-access-token/get-oauth-token'
+
+const gmailData: GmailData = {
+  serviceEmail: env.serviceEmail,
+  clientId: env.googleClientId as string,
+  clientSecret: env.googleSecret as string,
+  refreshToken: env.googleRefreshToken as string
+}
+
+export const makeResetPasswordEmailController = (): Controller => {
+  const validator = makeResetPasswordEmailValidator()
+  const accountMongoRepository = new AccountMongoRepository()
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const getOAuthToken = new GetOauthToken()
+  const transporter = new NodemailerGmailTransporter(gmailData, getOAuthToken)
+  const resetPasswordAccountRepository = new ResetPasswordAccountRepository()
+  const nodemailerAdapter = new NodemailerAdapter(env.serviceEmail, env.appUrl, transporter)
+  const dbResetPasswordEmail = new DbResetPasswordEmail(
+    accountMongoRepository,
+    nodemailerAdapter,
+    jwtAdapter,
+    resetPasswordAccountRepository,
+    resetPasswordAccountRepository,
+    resetPasswordAccountRepository)
+  return new ResetPasswordEmailController(validator, dbResetPasswordEmail)
+}
