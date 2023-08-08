56b082dfa1d43cda65755086a96b1e314b456e2f
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-08 18:50:36 -0300

    test: ensure verify-reset-password-token route return 401 if verify fails

---
+++ src/main/routes/reset-password-routes.spec.ts
@@ -0,0 +1,45 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/helpers/mongo-helper'
+import app from '../config/app'
+
+let accountsCollection: Collection
+describe('POST /send-reset-password-message', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  beforeEach(async () => {
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('should  return 200 on succeeds', async () => {
+    await accountsCollection.insertOne({
+      username: 'any_username',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    await request(app).post('/api/send-reset-password-email-message').send({ email: 'any_email@mail.com' }).expect(200)
+  })
+  test('should  return 401 if fails', async () => {
+    await request(app).post('/api/send-reset-password-email-message').send({ email: 'any_email@mail.com' }).expect(401)
+  })
+})
+
+describe('POST /verify-reset-password-token', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+  beforeEach(async () => {
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  test('should return 401 if verify fails', async () => {
+    await request(app).post('/api/verify-reset-password-token').send({ accessToken: 'any_token' }).expect(401)
+  })
+})
