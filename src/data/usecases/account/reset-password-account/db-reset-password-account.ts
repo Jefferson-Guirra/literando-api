de62ae03d0ec031af5f4f5681d31aa19f447712d
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-09 15:31:43 -0300

    feat: ensure DbResetPasswordAccount call Hasher with correct password

--- src/data/usecases/account/reset-password-account/db-reset-password-account.ts
+++ src/data/usecases/account/reset-password-account/db-reset-password-account.ts
@@ -1,13 +1,19 @@
 import { ResetPasswordAccount, ResetPasswordModel } from '../../../../domain/usecases/account/reset-password-account'
+import { Hasher } from '../../../protocols/criptography/hasher'
 import { LoadResetPasswordRequestByAccessTokenRepository } from '../../../protocols/db/email/load-reset-password-request-by-access-token-repository'
 
 export class DbResetPasswordAccount implements ResetPasswordAccount {
-  constructor (private readonly loadRequest: LoadResetPasswordRequestByAccessTokenRepository) {}
+  constructor (
+    private readonly loadRequest: LoadResetPasswordRequestByAccessTokenRepository,
+    private readonly hasher: Hasher
+  ) {}
+
   async resetPassword (accessToken: string, password: string): Promise<ResetPasswordModel | null> {
     const request = await this.loadRequest.loadRequestByAccessToken(accessToken)
     if (!request) {
       return null
     }
+    await this.hasher.hash(password)
     return { password: 'hashed_password' }
   }
 }
