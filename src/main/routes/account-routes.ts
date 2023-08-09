3e930ea652d6f746bb219a849a1e91bae2cb6fe6
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-09 19:43:17 -0300

    feat: creaate reset-password route

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -5,12 +5,14 @@ import { makeLoginController } from '../factories/account/login/login-controller
 import { makeLogoutController } from '../factories/account/logout/logout-controller'
 import { makeNextAuthSignupController } from '../factories/account/signup/next-auth-signup-factory'
 import { makeNextAuthLoginController } from '../factories/account/login/next-auth-login-controller'
+import { makeResetPasswordAccountController } from '../factories/account/reset-password/reset-password-account-controller-factory'
 
 const accountRouter = (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
   router.post('/next-auth-signup', adaptRoute(makeNextAuthSignupController()))
   router.put('/login', adaptRoute(makeLoginController()))
   router.put('/next-auth-login', adaptRoute(makeNextAuthLoginController()))
+  router.put('/reset-password', adaptRoute(makeResetPasswordAccountController()))
   router.put('/logout', adaptRoute(makeLogoutController()))
 }
 export default accountRouter
