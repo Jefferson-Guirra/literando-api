9866330f09fc1e30f05a44b133d4f31f5246ffb7
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-09 16:57:33 -0300

    refactor: create addAccountDatabase function fo add account in database

--- src/infra/db/account/account-mongo-repository.spec.ts
+++ src/infra/db/account/account-mongo-repository.spec.ts
@@ -1,8 +1,16 @@
-import { Collection } from 'mongodb'
+import { Collection, Document, InsertOneResult } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account-mongo-repository'
 
 const makeSut = (): AccountMongoRepository => new AccountMongoRepository()
+const addAccountDatabase = async (): Promise<InsertOneResult<Document>> => {
+  return await accountCollection.insertOne({
+    username: 'any_username',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    accessToken: 'any_token'
+  })
+}
 let accountCollection: Collection
 describe('AccountMongoRepository', () => {
   beforeAll(async () => {
@@ -18,17 +26,13 @@ describe('AccountMongoRepository', () => {
 
   test('should return account loadByEmail success', async () => {
     const sut = makeSut()
-    await accountCollection.insertOne({
-      username: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
-    })
+    await addAccountDatabase()
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeTruthy()
     expect(account?.id).toBeTruthy()
     expect(account?.email).toBe('any_email@mail.com')
     expect(account?.password).toBe('any_password')
-    expect(account?.username).toBe('any_name')
+    expect(account?.username).toBe('any_username')
   })
 
   test('should return null if loadByEmail fails', async () => {
@@ -68,26 +72,17 @@ describe('AccountMongoRepository', () => {
 
   test('should update the account accessToken on updateAccessToken success', async () => {
     const sut = makeSut()
-    const result = await accountCollection.insertOne({
-      username: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password'
-    })
+    const result = await addAccountDatabase()
     let account = await accountCollection.findOne({ _id: result.insertedId })
-    expect(account?.accessToken).toBeFalsy()
-    await sut.update(result.insertedId.toString(), 'any_token')
+    expect(account?.accessToken).toEqual('any_token')
+    await sut.update(result.insertedId.toString(), 'random_token')
     account = await accountCollection.findOne({ _id: result.insertedId })
-    expect(account?.accessToken).toBe('any_token')
+    expect(account?.accessToken).toBe('random_token')
   })
 
   test('should return account if loadByAccessToken success', async () => {
     const sut = makeSut()
-    const result = await accountCollection.insertOne({
-      username: 'any_username',
-      password: 'any_password',
-      email: 'any_email@mail.com',
-      accessToken: 'any_token'
-    })
+    const result = await addAccountDatabase()
     const account = await accountCollection.findOne({ _id: result.insertedId })
     const loadByAccessTokenAccount = await sut.loadByAccessToken(
       account?.accessToken
@@ -107,12 +102,7 @@ describe('AccountMongoRepository', () => {
 
   test('should remove accessToken if remove success', async () => {
     const sut = makeSut()
-    const result = await accountCollection.insertOne({
-      username: 'any_username',
-      password: 'any_password',
-      email: 'any_email@mail.com',
-      accessToken: 'any_token'
-    })
+    const result = await addAccountDatabase()
     let account = await accountCollection.findOne({ accessToken: 'any_token' })
     expect(account).toBeTruthy()
     expect(account?.accessToken).toBeTruthy()
@@ -126,12 +116,7 @@ describe('AccountMongoRepository', () => {
   })
   test('should update  password if resetPassword success', async () => {
     const sut = makeSut()
-    const result = await accountCollection.insertOne({
-      username: 'any_username',
-      email: 'any_email@mail.com',
-      password: 'any_password',
-      accessToken: 'any_token'
-    })
+    const result = await addAccountDatabase()
     const account = await sut.resetPassword('any_email@mail.com', 'random_password')
     const newAccount = await accountCollection.findOne({ _id: result.insertedId })
     expect(newAccount?.password).toEqual('random_password')
