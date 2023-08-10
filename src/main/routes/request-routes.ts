154564fcb4dc08f76ffa50729d5341a2eb829513
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-09 22:05:33 -0300

    refactor: rename file to request-routes

--- src/main/routes/request-routes.ts
+++ src/main/routes/request-routes.ts
@@ -3,9 +3,9 @@ import { makeResetPasswordEmailController } from '../factories/email/reset-passw
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeVerifyResetPasswordTokenController } from '../factories/email/verify-reset-password-token/verify-reset-password-token-controller-factory'
 
-const resetPasswordRoutes = (router: Router): void => {
+const requestRoutes = (router: Router): void => {
   router.post('/send-reset-password-email-message', adaptRoute(makeResetPasswordEmailController()))
   router.post('/verify-reset-password-token', adaptRoute(makeVerifyResetPasswordTokenController()))
 }
 
-export default resetPasswordRoutes
+export default requestRoutes
