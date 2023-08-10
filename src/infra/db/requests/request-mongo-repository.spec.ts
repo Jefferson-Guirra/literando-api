91b4fb03c1ef56b7713f0bd94006592dc5d036d3
Jefferson Guirra <jeffersonguirra8@gmail.com>
2023-08-09 22:21:49 -0300

    test: ensure RequestMongoRepository remove request if removeREqquest success

--- src/infra/db/requests/request-mongo-repository.spec.ts
+++ src/infra/db/requests/request-mongo-repository.spec.ts
@@ -2,7 +2,7 @@ import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { RequestMongoRepository } from './request-mongo-repository'
 
-let resetPasswordAccountsCollection: Collection
+let requestsCollection: Collection
 const makeSut = (): RequestMongoRepository => new RequestMongoRepository()
 
 describe('RequestMongoRepository', () => {
@@ -10,18 +10,18 @@ describe('RequestMongoRepository', () => {
     await MongoHelper.connect(process.env.MONGO_URL as string)
   })
   beforeEach(async () => {
-    resetPasswordAccountsCollection = await MongoHelper.getCollection('resetPasswordAccounts')
-    await resetPasswordAccountsCollection.deleteMany({})
+    requestsCollection = await MongoHelper.getCollection('resetPasswordAccounts')
+    await requestsCollection.deleteMany({})
   })
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
   test('should return account data if add success', async () => {
     const sut = makeSut()
-    let count = await resetPasswordAccountsCollection.countDocuments()
+    let count = await requestsCollection.countDocuments()
     expect(count).toBe(0)
     const response = await sut.add('any_email@mail.com', 'any_token')
-    count = await resetPasswordAccountsCollection.countDocuments()
+    count = await requestsCollection.countDocuments()
     expect(count).toBe(1)
     expect(response).toBeTruthy()
     expect(response?.id).toBeTruthy()
@@ -30,7 +30,7 @@ describe('RequestMongoRepository', () => {
   })
   test('should return account data if GetResetPasswordRequestRepository success', async () => {
     const sut = makeSut()
-    await resetPasswordAccountsCollection.insertOne({ email: 'any_email@mail.com', accessToken: 'any_token' })
+    await requestsCollection.insertOne({ email: 'any_email@mail.com', accessToken: 'any_token' })
     const response = await sut.loadRequestByEmail('any_email@mail.com')
     expect(response).toBeTruthy()
     expect(response?.id).toBeTruthy()
@@ -44,7 +44,7 @@ describe('RequestMongoRepository', () => {
   })
   test('should return account data if UpdateResetPasswordTokenRepository success', async () => {
     const sut = makeSut()
-    await resetPasswordAccountsCollection.insertOne({ email: 'any_email@mail.com', accessToken: 'any_token' })
+    await requestsCollection.insertOne({ email: 'any_email@mail.com', accessToken: 'any_token' })
     const response = await sut.update('any_email@mail.com', 'random_token')
     expect(response).toBeTruthy()
     expect(response?.id).toBeTruthy()
@@ -63,11 +63,23 @@ describe('RequestMongoRepository', () => {
   })
   test('should return account data on succeeds', async () => {
     const sut = makeSut()
-    await resetPasswordAccountsCollection.insertOne({ email: 'any_email@mail.com', accessToken: 'any_token' })
+    await requestsCollection.insertOne({ email: 'any_email@mail.com', accessToken: 'any_token' })
     const response = await sut.loadRequestByAccessToken('any_token')
     expect(response).toBeTruthy()
     expect(response?.id).toBeTruthy()
     expect(response?.email).toBe('any_email@mail.com')
     expect(response?.accessToken).toBe('any_token')
+  })
+  test('should remove request  if removeRequest success', async () => {
+    const sut = makeSut()
+    await requestsCollection.insertOne({
+      email: 'any_email@mail.com',
+      accessToken: 'any_token'
+    })
+    let count = await requestsCollection.countDocuments()
+    expect(count).toBe(1)
+    await sut.removeRequest('any_token')
+    count = await requestsCollection.countDocuments()
+    expect(count).toBe(0)
   })
 })
